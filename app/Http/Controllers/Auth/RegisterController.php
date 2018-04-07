274206a87f8c5c2a47ce86c9ea7f61576d7c9c25
hyb <354037968@qq.com>
2018-04-07 21:29:25 +0800

    no message

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/dashboard/home';
 
     /**
      * Create a new controller instance.
